311c751680402d70e66a404fe83a8d0ad1c630a7
Mark McLoughlin <markmc@redhat.com>
2020-02-24 12:52:54 +0000

    Copy Provisioning CRD from machine-api-operator
    Using the version in commit c1885a91
    
    Run `operator-sdk generate crds`

--- pkg/apis/metal3/v1alpha1/provisioning_types.go
+++ pkg/apis/metal3/v1alpha1/provisioning_types.go
@@ -2,28 +2,20 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	operatorv1 "github.com/openshift/api/operator/v1"
 )
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// ProvisioningSpec defines the desired state of Provisioning
-type ProvisioningSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
-}
-
-// ProvisioningStatus defines the observed state of Provisioning
-type ProvisioningStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
-}
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// Provisioning is the Schema for the provisionings API
+// Provisioning contains configuration used by the Provisioning
+// service (Ironic) to provision baremetal hosts.
+// Provisioning is created by the OpenShift installer using admin or
+// user provided information about the provisioning network and the
+// NIC on the server that can be used to PXE boot it.
+// This CR is a singleton, created by the installer and currently only
+// consumed by the cluster-baremetal-operator to bring up and update
+// containers in a metal3 cluster.
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:path=provisionings,scope=Namespaced
 type Provisioning struct {
@@ -32,6 +24,62 @@ type Provisioning struct {
 
 	Spec   ProvisioningSpec   `json:"spec,omitempty"`
 	Status ProvisioningStatus `json:"status,omitempty"`
+}
+
+// ProvisioningSpec defines the provisioning configuration for Metal3.
+type ProvisioningSpec struct {
+	// ProvisioningInterface is the name of the network interface
+	// on a baremetal server to the provisioning network. It can
+	// have values like eth1 or ens3.
+	ProvisioningInterface string `json:"provisioningInterface,omitempty"`
+
+	// ProvisioningIP is the IP address assigned to the
+	// provisioningInterface of the baremetal server. This IP
+	// address should be within the provisioning subnet, and
+	// outside of the DHCP range.
+	ProvisioningIP string `json:"provisioningIP,omitempty"`
+
+	// ProvisioningNetworkCIDR is the network on which the
+	// baremetal nodes are provisioned. The provisioningIP and the
+	// IPs in the dhcpRange all come from within this network.
+	ProvisioningNetworkCIDR string `json:"provisioningNetworkCIDR,omitempty"`
+
+	// ProvisioningDHCPExternal indicates whether the DHCP server
+	// for IP addresses in the provisioning DHCP range is present
+	// within the metal3 cluster or external to it.
+	ProvisioningDHCPExternal bool `json:"provisioningDHCPExternal,omitempty"`
+
+	// ProvisioningDHCPRange needs to be interpreted along with
+	// ProvisioningDHCPExternal. If the value of
+	// provisioningDHCPExternal is set to False, then
+	// ProvisioningDHCPRange represents the range of IP addresses
+	// that the DHCP server running within the metal3 cluster can
+	// use while provisioning baremetal servers. If the value of
+	// ProvisioningDHCPExternal is set to True, then the value of
+	// ProvisioningDHCPRange will be ignored. When the value of
+	// ProvisioningDHCPExternal is set to False, indicating an
+	// internal DHCP server and the value of ProvisioningDHCPRange
+	// is not set, then the DHCP range is taken to be the default
+	// range which goes from .10 to .100 of the
+	// ProvisioningNetworkCIDR. This is the only value in all of
+	// the Provisioning configuration that can be changed after
+	// the installer has created the CR. This value needs to be
+	// two comma sererated IP addresses within the
+	// ProvisioningNetworkCIDR where the 1st address represents
+	// the start of the range and the 2nd address represents the
+	// last usable address in the  range.
+	ProvisioningDHCPRange string `json:"provisioningDHCPRange,omitempty"`
+
+	// ProvisioningOSDownloadURL is the location from which the OS
+	// Image used to boot baremetal host machines can be
+	// downloaded by the metal3 cluster.
+	ProvisioningOSDownloadURL string `json:"provisioningOSDownloadURL,omitempty"`
+}
+
+// ProvisioningStatus defines the observed values from the
+// cluster. They may not be overridden.
+type ProvisioningStatus struct {
+	operatorv1.OperatorStatus `json:",inline"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
