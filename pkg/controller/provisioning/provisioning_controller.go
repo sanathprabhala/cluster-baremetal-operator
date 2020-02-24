0a36957225ffff4395e9573073ac50fc4172d0ab
Mark McLoughlin <markmc@redhat.com>
2020-02-24 14:07:03 +0000

    Remove scaffolding comment

--- pkg/controller/provisioning/provisioning_controller.go
+++ pkg/controller/provisioning/provisioning_controller.go
@@ -95,11 +95,6 @@ type ReconcileProvisioning struct {
 
 // Reconcile reads that state of the cluster for a Provisioning object and makes changes based on the state read
 // and what is in the Provisioning.Spec
-// TODO(user): Modify this Reconcile function to implement your Controller logic.  This example creates
-// a Pod as an example
-// Note:
-// The Controller will requeue the Request to be processed again if the returned error is non-nil or
-// Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
 func (r *ReconcileProvisioning) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
 	reqLogger.Info("Reconciling Provisioning")
