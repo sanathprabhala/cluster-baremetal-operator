d8ae2fbdb268adf99049390b64123e702a61ea97
Mark McLoughlin <markmc@redhat.com>
2020-02-24 12:52:45 +0000

    Auto-add Provisioning CRD
    By running:
    
    $> operator-sdk add api --kind=Provisioning --api-version=metal3.io/v1alpha1

---
+++ pkg/apis/metal3/group.go
@@ -0,0 +1,6 @@
+// Package metal3 contains metal3 API versions.
+//
+// This file ensures Go source parsers acknowledge the metal3 package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package metal3
